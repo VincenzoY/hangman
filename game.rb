8a8d9a612625a525d3304b64453435c6d34e84e7
VincenzoY <vincenzoyee@gmail.com>
2019-12-11 20:20:12 +0000

    Added save feature but still have to implement overwriting of loaded files

--- game.rb
+++ game.rb
@@ -5,33 +5,30 @@ class Game
   end
 
   def play_game
-    puts
-    puts "Choose a letter"
-    puts @player.guessed.join(" ")
     while (@player.wrong != @player.tries && @player.guessed.include?("_")) do
+      puts
+      puts "Choose a letter"
+      puts @player.guessed.join(" ")
+      puts "Previous Guesses: #{@player.previous_guesses.join(", ")}"
+      puts "You've guessed #{@player.wrong} letters wrong. You have #{@player.tries - @player.wrong} wrong guesses left"
+      puts "Press (1) to save the game."
       letter = gets.chomp.downcase
       if letter == "1"
         break
       end
-      while letter.length > 1 || !letter.is_a?(String) || letter == "" do
-        puts "That's not a valid character. Try again."
+      while letter.length > 1 || !letter.is_a?(String) || letter == "" || @player.previous_guesses.include?(letter) do
+        puts "That's not a valid character or it is already used. Try again."
         letter = gets.chomp.downcase
       end
       puts
       if @player.word.include?(letter)
-        puts "Nice job! Keep on guessing"
+        puts "Nice job!"
         @player.guessed[@player.word.index(letter)] = letter
       else
-        puts "Nice try! You'll get it next time"
-      end
-      puts @player.guessed.join(" ")
-      unless @player.previous_guesses.include?(letter)
-        @player.previous_guesses << letter 
+        puts "Nice try!"
         @player.wrong += 1
       end
-      puts "Previous Guesses: #{@player.previous_guesses.join(", ")}"
-      puts "You've guessed #{@player.wrong} letters wrong. You have #{@player.tries - @player.wrong} wrong guesses left"
-      puts "Press (1) to save the game."
+      @player.previous_guesses << letter 
     end
     puts
     if letter == "1"
@@ -44,6 +41,6 @@ class Game
   end
 
   def save
-    File.open("saves/#{@player.player}.txt", 'w') {|file| file.puts "test"}
+      File.open("saves.txt", 'a'){|f| f.puts "#{@player.player}; #{@player.word}; #{@player.guessed.join}; #{@player.previous_guesses.join}; #{@player.wrong}"}
   end
 end
--- main.rb
+++ main.rb
@@ -14,20 +14,51 @@ class Hangman
     if choice == "0"
       puts "What's your name?"
       player = gets.chomp
-      while player.length < 1 || !player.is_a?(String) do
-        puts "That's not a name. Try again"
+      while player.length < 1 || !player.is_a?(String) || File.exists?("saves/#{player}.txt") do
+        puts "That's not a valid name or it is already in use. Try again"
         player = gets.chomp
       end
-      player = Player.new(player, Array.new, Array.new, 0)
+      generate_word
+      player = Player.new(player, @word, @guessed, Array.new, 0)
       Game.new(player)
     elsif choice == "1"
-      load_game
+      puts "Which save would you like to load? (Type the Number)"
+      saves = File.readlines "saves.txt"
+      saves.each_with_index do |line, index|
+        next if index == 0
+        columns = line.split("; ")
+        @list = Array.new
+        @list << "#{index}: #{columns[0]}"
+        puts @list
+      end
+      @choice = gets.chomp
+      while @choice.to_i > @list.length || @choice.to_i <= 0 || @choice.include?(".")do
+        puts "That's not a valid number. Try again"
+        @choice = gets.chomp
+      end
+      saves.each_with_index do |line, index|
+        next unless index == @choice.to_i
+        columns = line.split("; ")
+        player = Player.new(columns[0], columns[1], columns[2].split(""), columns[3].split(""), columns[4].to_i)
+        Game.new(player)
+      end
     else
       puts "That's not a valid answer. Please try again"
       welcome
     end
   end
 
+  def generate_word
+    dictionary = File.readlines "5desk.txt"
+    dictionary.each_with_index do |line, index|
+      @word = dictionary[rand(dictionary.length)].chomp.downcase
+    end
+    @guessed = Array.new
+    (@word.length-1).times { @guessed << "_" }
+    if @word.length < 6 || @word.length > 12
+      generate_word
+    end
+  end
 end
 
 x = Hangman.new
