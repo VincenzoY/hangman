4be9468fb33d7604ab08e9f758857c87375542d9
VincenzoY <vincenzoyee@gmail.com>
2019-12-11 20:51:30 +0000

    Save and delete functions completely added. Hangman is completed

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require './game.rb'
 require './player.rb'
+require 'fileutils'
 
 class Hangman
   def initialize
@@ -8,7 +9,7 @@ class Hangman
 
   def welcome
     puts "Welcome to Hangman"
-    puts "Would you like to start a new game (0) or load a previous save (1)"
+    puts "Would you like to start a new game (0) or load a previous save (1). To delete a file, load it but don't save it."
     choice = gets.chomp
     puts
     if choice == "0"
@@ -22,16 +23,19 @@ class Hangman
       player = Player.new(player, @word, @guessed, Array.new, 0)
       Game.new(player)
     elsif choice == "1"
-      puts "Which save would you like to load? (Type the Number)"
+      puts "Which save would you like to load? (Type the Number). Press any key to start again."
       saves = File.readlines "saves.txt"
       saves.each_with_index do |line, index|
         next if index == 0
         columns = line.split("; ")
         @list = Array.new
-        @list << "#{index}: #{columns[0]}"
+        @list << "#{index}: #{columns[0]} (#{columns[2]})"
         puts @list
       end
       @choice = gets.chomp
+      puts
+      welcome if @choice.to_i == 0
+      welcome if @list == nil
       while @choice.to_i > @list.length || @choice.to_i <= 0 || @choice.include?(".")do
         puts "That's not a valid number. Try again"
         @choice = gets.chomp
@@ -40,6 +44,7 @@ class Hangman
         next unless index == @choice.to_i
         columns = line.split("; ")
         player = Player.new(columns[0], columns[1], columns[2].split(""), columns[3].split(""), columns[4].to_i)
+        overwrite
         Game.new(player)
       end
     else
@@ -54,10 +59,19 @@ class Hangman
       @word = dictionary[rand(dictionary.length)].chomp.downcase
     end
     @guessed = Array.new
-    (@word.length-1).times { @guessed << "_" }
+    @word.length.times { @guessed << "_" }
     if @word.length < 6 || @word.length > 12
       generate_word
     end
+  end
+
+  def overwrite
+    File.open('output.txt', 'w') do |out_file|
+      File.foreach('saves.txt').with_index do |line,line_number|
+      out_file.puts line unless line_number == @choice.to_i
+      end
+    end
+    FileUtils.mv('output.txt', 'saves.txt')
   end
 end
 
